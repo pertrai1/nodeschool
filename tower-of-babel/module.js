94a98006244214fca2eaa57b16c7ba41ae0b6e47
Rob Simpson <robsimpson@Robs-MacBook-Pro-2.local>
2017-05-01 17:58:14 -0400

    start of tower of babel

---
+++ tower-of-babel/module.js
@@ -0,0 +1,6 @@
+const PI = 3.141592;
+let _sqrt = (s, x, last) => x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
+let sqrt = s => _sqrt(s, s / 2.0, 0.0);
+let square = x => x * x;
+
+export {PI, sqrt, square};
---
+++ tower-of-babel/template-strings.js
@@ -0,0 +1,5 @@
+//var a = "Hello Babel";
+//console.log(`${a}`);
+
+var arg = process.argv[2];
+console.log(`Hello ${arg}`);
